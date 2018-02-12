c25e897667c250668874c0bcce2693267504e4c0
rekikhaile <rekikhaile@gmail.com>
2018-02-12 17:22:57 -0500

    Initial commit

---
+++ src/main/java/com/riri/springsecuritybasic/SpringsecuritybasicApplication.java
@@ -0,0 +1,12 @@
+package com.riri.springsecuritybasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritybasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecuritybasicApplication.class, args);
+	}
+}
